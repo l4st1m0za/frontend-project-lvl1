50809f8228791694fbf1a1f08ce520e9ad5f7100
Roshan Jha <kartohajha@gmail.com>
2021-01-25 03:00:32 +0530

    making brain-even better

--- src/logicForEven.js
+++ src/logicForEven.js
@@ -4,34 +4,45 @@ const foo = () => {
     const userName = readlineSync.question('May I have your name? ');
     console.log('Hello, ' + userName)
     console.log(`Answer "yes" if the number is even, otherwise answer "no".`)
-    console.log('Question: 15')
+
+
+    const ranNum1 = Math.floor((Math.random() * 10) + Math.random() * 10)
+    console.log('Question: ' + ranNum1)
     const firstAnsw = readlineSync.question('Your answer: ');
 
-    if (firstAnsw === 'no') {
+    if (ranNum1 % 2 !== 0 && firstAnsw === 'no' || ranNum1 % 2 === 0 && firstAnsw === 'yes') {
         console.log('Correct!')
     } else {
         console.log(`'${firstAnsw}' is wrong answer ;(. Correct answer was 'no'.`)
         return 'Let\'s try again, ' + userName
     };
 
-    console.log('Question: 8')
+
+    const ranNum2 = Math.floor((Math.random() * 10) + Math.random() * 10)
+    console.log('Question: ' + ranNum2)
     const secondAnsw = readlineSync.question('Your answer: ');
-    if (secondAnsw === 'yes') {
+
+    if (ranNum2 % 2 !== 0 && secondAnsw === 'no' || ranNum2 % 2 === 0 && secondAnsw === 'yes') {
         console.log('Correct!')
     } else {
-        console.log(`'${secondAnsw}' is wrong answer ;(. Correct answer was 'yes'.`)
+        console.log(`'${secondAnsw}' is wrong answer ;(. Correct answer was 'no'.`)
         return 'Let\'s try again, ' + userName
     };
 
-    console.log('Question: 6')
+
+    const ranNum3 = Math.floor((Math.random() * 10) + Math.random() * 10)
+    console.log('Question: ' + ranNum3)
     const thirdAnsw = readlineSync.question('Your answer: ');
-    if (thirdAnsw === 'yes') {
+
+    if (ranNum3 % 2 !== 0 && thirdAnsw === 'no' || ranNum3 % 2 === 0 && thirdAnsw === 'yes') {
         console.log('Correct!')
         return `Congratulations, ` + userName + '!'
+
     } else {
-        console.log(`'${thirdAnsw}' is wrong answer ;(. Correct answer was 'yes'.`)
+        console.log(`'${thirdAnsw}' is wrong answer ;(. Correct answer was 'no'.`)
         return 'Let\'s try again, ' + userName
     };
+
 };
 
 export default foo;
